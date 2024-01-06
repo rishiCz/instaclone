31278353f9b0406aeac9c92a5b9cbb50696ce17a
Rishab <rishabkanaujia2002@gmail.com>
2024-01-06 23:16:30 +0530

    first commit

---
+++ routes/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
+const postSchema = mongoose.Schema({
+  picture:String,
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user"
+  },
+  caption:String,
+  Date:{
+    type:Date,
+    default:Date.now
+  },
+  likes:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user"
+  }],
+});
+
+
+module.exports = mongoose.model("post", postSchema); 
+
+
+

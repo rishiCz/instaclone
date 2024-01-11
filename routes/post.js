afd22f40c1492feec3954bf6c6775ae04f589371
Rishab <rishabkanaujia2002@gmail.com>
2024-01-11 23:19:25 +0530

    first commit

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
+mongoose.connect(process.env.MONGO);;
 const postSchema = mongoose.Schema({
   picture:String,
   user:{
